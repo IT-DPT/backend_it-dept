d29ea9ccbf085c193397a672dcd598dab7bf3f86
ChavanDr99 <chavandipesh399@gmail.com>
2023-10-22 00:44:18 +0530

    push

--- routes/Admin.js
+++ routes/Admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("express");
 const cloudinary = require("cloudinary").v2;
 const adminModel=require('../Models/AdminModel')
 const studentModel = require("../Models/Student");
